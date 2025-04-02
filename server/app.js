de553817fe7344c3c602663d1019b77c7f22b73c
David <77506410+gdavidev@users.noreply.github.com>
2025-04-02 00:06:55 -0300

    fix: create database directory if it doesn't exist

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const dataAccess = require('./data/DataAccess');
 
 const configurationsRoutes = require('./routes/configurationsRoutes');
@@ -13,6 +14,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client/dist/')));
 
+if (!fs.existsSync(path.join(__dirname, './database')))
+  fs.mkdirSync(path.join(__dirname, './database'));
 dataAccess.initialize(path.join(__dirname, './database/quiz.sqlite'))
 
 app.use('/api', configurationsRoutes);
