21487c0a82e9fd20bddc0d61cb14b0f9fc496adb
David <77506410+gdavidev@users.noreply.github.com>
2025-04-07 22:54:03 -0300

    fix

---
+++ environment.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+        {
+            name: "quiz",
+            cwd: "/root/quiz/Quiz-Machine-Nodejs/",
+            script: "npm",
+            args: "node:start",
+            interpreter: "none",
+            watch: false
+        }
+    ]
+};
